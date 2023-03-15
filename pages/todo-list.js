a3b01115b195305efa24d27983fa17b9d87394c5
Daniela Küster <daniela.kuester@gmx.net>
2023-03-15 12:59:16 +0100

    Add: Functons for input field and submit button

--- pages/todo-list.js
+++ pages/todo-list.js
@@ -1,18 +1,32 @@
+import { useState } from "react";
+
 export default function TodoList() {
+    const [todo, setTodo] = useState("");
+
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        console.log(todo);
+        e.preventDefault();
+    }
+
     return(
         <div className="h-screen bg-gradient-to-br from-green-300 via-blue-400 to-purple-500">
             <h1 className="text-3xl p-8 font-bold text-white text-center">
                 My To-Do-List
             </h1>
-            <form className="container flex mx-auto w-1/2">
+            <form onSubmit={handleSubmit} className="container flex mx-auto w-1/2">
                 <input
                     className="mb-10 ml-3 pl-2 h-10 w-full text-black text-2xl"
                     type="text"
                     name="newtodo"
                     placeholder="Add a new task here."
+                    value={todo}
+                    onChange={handleChange}
                 />
                 <button type="submit"
-                    class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl
+                    className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl
                     focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800
                     font-medium rounded-lg text-2xl px-5 py-1 text-center ml-5 mr-2 mb-2 h-10">
                     Submit
