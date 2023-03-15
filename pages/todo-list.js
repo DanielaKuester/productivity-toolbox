bfc81fdc30d0cfc08f1ec2fa95ec156ef84aca89
Daniela Küster <daniela.kuester@gmx.net>
2023-03-15 11:34:56 +0100

    Add: input field with submit button

--- pages/todo-list.js
+++ pages/todo-list.js
@@ -1,10 +1,24 @@
 export default function TodoList() {
     return(
-        <div className="h-screen bg-gradient-to-br from-green-500 via-blue-700 to-purple-700">
+        <div className="h-screen bg-gradient-to-br from-green-300 via-blue-400 to-purple-500">
             <h1 className="text-3xl p-8 font-bold text-white text-center">
                 My To-Do-List
             </h1>
-            <ul className="list-decimal container mx-auto box-border rounded-xl shadow-2xl h-auto w-1/2 p-4 px-10 bg-opacity-60 bg-gray-700 flex flex-col text-left text-2xl">
+            <form className="container flex mx-auto w-1/2">
+                <input
+                    className="mb-10 ml-3 pl-2 h-10 w-full text-black text-2xl"
+                    type="text"
+                    name="newtodo"
+                    placeholder="Add a new task here."
+                />
+                <button type="submit"
+                    class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl
+                    focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800
+                    font-medium rounded-lg text-2xl px-5 py-1 text-center ml-5 mr-2 mb-2 h-10">
+                    Submit
+                </button>
+            </form>
+            <ul className="list-decimal container mx-auto box-border rounded-xl shadow-2xl h-auto w-1/2 p-4 px-10 bg-opacity-20 bg-gray-700 flex flex-col text-left text-2xl text-white">
                 <li className="p-1 line-through">
                     Experiment with Tailwind CSS
                 </li>
