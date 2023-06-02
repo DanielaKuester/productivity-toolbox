6f1f8245ad24c8a7390c942620dd98231325195e
Daniela Küster <daniela.kuester@gmx.net>
2023-06-02 16:59:33 +0200

    Style: Make design cleaner

--- pages/pomodoro.js
+++ pages/pomodoro.js
@@ -16,41 +16,41 @@ export default function Pomodoro() {
                     <div className="bg-transparent min-h-[60px] border border-0 col-span-6 text-center justify-center text-8xl pt-1 p-3 mb-1 z-10">
                         <Image src={"Peeking-Cat.svg"} alt={"image of a peeping cat"} width={600} height={200}/>
                     </div>
-                    <div className="bg-blue-100 min-h-[60px] border border-black col-span-6 text-center justify-center text-8xl pt-1 p-3 -mt-32">20:00</div>
-                    <div className="bg-blue-200 min-h-[60px] border border-black border-t-0 col-span-6 justify-between">
-                        <button className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl
+                    <div className="bg-white min-h-[60px] border border-black col-span-6 text-center justify-center text-8xl pt-1 p-3 -mt-32 rounded-t-3xl">20:00</div>
+                    <div className="bg-blue-100 min-h-[60px] border border-black border-t-0 col-span-6 justify-between">
+                        <button className="text-black bg-blue-300 hover:bg-blue-400 border border-black
                         focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-500
-                        font-medium rounded-lg text-2xl px-5 py-1 text-center h-10 m-3 w-40">Start/Pause</button>
-                        <button className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl
+                        font-medium text-2xl px-5 py-2 text-center h-12 m-3 ml-8 w-40">Start/Pause</button>
+                        <button className="text-black bg-blue-300 hover:bg-blue-400 border border-black
                         focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-500
-                        font-medium rounded-lg text-2xl px-5 py-1 text-centerh-10 m-3 w-40">Stop</button>
-                        <button className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl
+                        font-medium text-2xl px-5 py-2 text-center h-12 m-3 ml-8 w-40">Stop</button>
+                        <button className="text-black bg-blue-300 hover:bg-blue-400 border border-black
                         focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-500
-                        font-medium rounded-lg text-2xl px-5 py-1 text-centerh-10 m-3 w-40">Reset</button>
+                        font-medium text-2xl px-5 py-2 text-center h-12 m-3 ml-8 w-40">Reset</button>
                     </div>
-                    <div className="bg-blue-300 min-h-[60px] border border-black border-t-0 col-span-3 border-r-0 text-xl">
+                    <div className="bg-blue-200 min-h-[60px] border border-black border-t-0 col-span-3 border-r-0 rounded-bl-3xl text-xl pb-4">
                         <form>
-                            <div className="m-3 mt-4">
+                            <div className="m-3 ml-5 mt-4">
                                 <label className="mr-1">Work (in min):</label>
-                                <input type="number" min="1" max="180" className="float-right mr-4 w-24 border border-black"></input>
+                                <input type="number" min="1" max="180" className="float-right mr-2 w-24 border border-black"></input>
                             </div>
-                            <div className="m-3">
+                            <div className="m-3 ml-5">
                                 <label>Short Break (in min):</label>
-                                <input type="number" min="1" max="180" className="float-right mr-4 w-24 border border-black"></input>
+                                <input type="number" min="1" max="180" className="float-right mr-2 w-24 border border-black"></input>
                             </div>
-                            <div className="m-3">
+                            <div className="m-3 ml-5">
                                 <label>Long Break (in min):</label>
-                                <input type="number" min="1" max="180" className="float-right mr-4 w-24 border border-black"></input>
+                                <input type="number" min="1" max="180" className="float-right mr-2 w-24 border border-black"></input>
                             </div>
                             <button type="submit"
-                                className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl
+                                className="text-black bg-blue-300 hover:bg-blue-400 border border-black
                                 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-500
-                                font-medium rounded-lg text-2xl px-5 py-1 text-center mt-2 mb-4 m-3 h-10">
-                                Set times (in minutes)
+                                font-medium text-2xl px-5 py-2 ml-5 text-center h-12 m-3 w-40">
+                                Set times
                             </button>
                         </form>
                     </div>
-                    <div className="bg-blue-300 min-h-[60px] border border-black border-t-0 col-span-3 text-xl">
+                    <div className="bg-blue-200 min-h-[60px] border border-black border-t-0 col-span-3 rounded-br-3xl p-3 text-xl">
                         <h2 className="mt-1 ml-2 underline">Current Task:</h2>
                         <p className="mt-1 ml-2">Find out how CSS Grid woks best and apply those experimental changes to my pomodoro timer.
                         </p>
