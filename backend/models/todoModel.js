2fea67c5d9c995a5842a4db702c3159215cd62fa
Daniela Küster <daniela.kuester@gmx.net>
2023-08-08 19:03:52 +0200

    Add: Commentary that explains timestamp

--- backend/models/todoModel.js
+++ backend/models/todoModel.js
@@ -6,7 +6,8 @@ const todoSchema = mongoose.Schema({
         required: [true, 'Please add a text value']
     }
 },
-{
+{   
+    // Adds a timestamp to the todo item. Can also be left out but might be helpful later for the progress diary/log feature.
     timestamps: true,
 })
 
