ac602c682882560819d172e2d2e8ad1b9073bc42
Daniela Küster <daniela.kuester@gmx.net>
2023-08-08 17:32:46 +0200

    Add: Async express handler for async requests

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler') // Add express async handler for async requests
+
 // @desc    Get todos
 // @route   GET /api/todos
 // @access  public ==> later private for all GET/POST/PUT/DELETE requests --> after authentification, login, signup is added)
-const getTodos = (req, res) => {
+const getTodos = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @desc    Set todo
 // @route   POST /api/todos
 // @access  public
-const setTodo = (req, res) => {
+const setTodo = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
 
     res.status(200).json({ message: 'Set goal' })
-}
+})
 
 // @desc    Update todo
 // @route   PUT /api/todos/:id
 // @access  public
-const updateTodo = (req, res) => {
+const updateTodo = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update todo ${req.params.id}`});
-}
+})
 
 // @desc    Delete todo
 // @route   DELETE /api/todos/:id
 // @access  public
-const deleteTodo = (req, res) => {
+const deleteTodo = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete todo ${req.params.id}`});
-}
+})
 
 module.exports = {
     getTodos,
