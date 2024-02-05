5e41a557280bc31faf84011f8c5ea113d4d0bb49
Daniela Küster <daniela.kuester@gmx.net>
2024-02-05 16:21:43 +0100

    Add: "Add Task" button

--- components/TodoForm.jsx
+++ components/TodoForm.jsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { createTodo } from "../src/features/todos/todoSlice"
 
 function TodoForm() {
     const [text, setText] = useState("");
 
+    const dispatch = useDispatch();
+
     const onSubmit = e => {
         e.preventDefault();
+
+        dispatch(createTodo({text}));
+        // Clear the form by setting it to an empty string.
+        setText("");
     }
 
     return (
         <section className="form">
             <form onSubmit={onSubmit}>
                 <div className="form-group">
-                    <label htmlFor="text" className="pr-4 text-2xl">To Do:</label>
+                    <label htmlFor="text" className="-ml-20 pr-4 text-2xl">To Do:</label>
                     <input
                         type="text"
                         className="p-2 mt-4 w-96 border-solid border-4 border-gray-400"
@@ -22,6 +29,11 @@ function TodoForm() {
                         // When the user types something, it is displayed in the text field.
                         onChange={(e) => setText(e.target.value)}
                     />
+                </div>
+                <div className="form-group">
+                    <button className="p-2 mt-4 w-96 bg-gray-800 text-white text-lg rounded" type="submit">
+                        Add Task
+                    </button>
                 </div>
             </form>
         </section>
