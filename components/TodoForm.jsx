e50254dcd4b4293c56ad5ce773a6add1f48c8ec2
Daniela Küster <daniela.kuester@gmx.net>
2024-02-05 15:39:49 +0100

    Add: First proper to do form field

--- components/TodoForm.jsx
+++ components/TodoForm.jsx
@@ -2,8 +2,29 @@ import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function TodoForm() {
+    const [text, setText] = useState("");
+
+    const onSubmit = e => {
+        e.preventDefault();
+    }
+
     return (
-        <div>TodoForm</div>
+        <section className="form">
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label htmlFor="text" className="pr-4 text-2xl">To Do:</label>
+                    <input
+                        type="text"
+                        className="p-2 mt-4 w-96 border-solid border-4 border-gray-400"
+                        name="text"
+                        id="text"
+                        value={text}
+                        // When the user types something, it is displayed in the text field.
+                        onChange={(e) => setText(e.target.value)}
+                    />
+                </div>
+            </form>
+        </section>
     )
 }
 
